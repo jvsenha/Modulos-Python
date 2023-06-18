00d399d7eb118943b58e8fd597930fd5d1d4eea6
jvsenha <joao.santos1381@etec.sp.gov.br>
2023-06-18 16:39:38 -0300

    teste if

--- Modulos/Test_git.py
+++ Modulos/Test_git.py
@@ -1,1 +1,8 @@
-print('olá Mundo!')
+n1=int(input("digite um numero"))
+n2=int(input("digite outro numero"))
+if n1<n2:
+    print(n2)
+elif n2<n1:
+    print(n1)
+else:
+    print(" numeros são iguais")
